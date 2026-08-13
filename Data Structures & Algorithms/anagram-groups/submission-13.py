db5060c8ba24c30e2305c3675b3d2bd72ab1bba6
Sean Brasse <Seanbrasse@gmail.com>
2026-08-13 09:42:47 -0400

    Add: anagram-groups - submission-13

---
+++ Data Structures & Algorithms/anagram-groups/submission-13.py
@@ -0,0 +1,10 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        seen = defaultdict(list)
+
+        for string in strs:
+            sorted_string = ''.join(sorted(string))
+            seen[sorted_string].append(string)
+        
+        return list(seen.values())
+        
