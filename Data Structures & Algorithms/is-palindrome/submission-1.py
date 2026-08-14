5135c15e9a70877167cb149bd146aafd3f92473f
Sean Brasse <Seanbrasse@gmail.com>
2026-08-14 16:44:28 -0400

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        alphanumeric = ''.join(char for char in s if char.isalnum()).lower()
+        a, b = 0, len(alphanumeric) - 1
+        
+        while a < b:
+            if alphanumeric[a] != alphanumeric[b]:
+                return False
+            a += 1
+            b -= 1
+        return True
+
+
+        
